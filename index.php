0bd468a3f96fa18a34e757c266cee2f6abae4ead
Nafion Antoy <antoynafion@gmail.com>
2022-08-11 12:47:27 +0200

    Ajout du fichier style.css et mise en place du squelette en html.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hergeur d'image</title>
+</head>
+<body>
+    <header>
+        <h1>Hébergeur d'image</h1>
+    </header>
+        <section id="conteneurImage">
+            <p>Aucune photo reçu pour le moment...</p>
+        </section>
+        <section id="traitement">
+            <aside>
+                <form action="">
+                    <input type="file"></br>
+                    <input type="submit" value="Envoyer">
+                </form>
+            </aside>
+            <aside></aside>
+
+        </section>
+</body>
+</html>
